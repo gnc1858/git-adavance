da3ed1ae1162705f00e936623e014a9b114f576a
gnc1858 <genct1858@gmail.com>
2021-10-02 06:22:37 +0300

    add Animal class
    Animal class added to create diffrent animals

--- components/app.js
+++ components/app.js
@@ -1,7 +1,18 @@
 console.log("Here we are")
 
-setInterval(()=>{
-   console.log("some message")
-},4000)
+class Animal{
+   constructor(name){
+      this.name=name
+   }
+
+   presentAnimal(message){
+     console.log(`The ${this.name} says : ${message}`)
+   }
+
+}
+
+
+let eagle=new Animal("eagle");
+eagle.presentAnimal("I love flying, I have sharp eyes")
 
 
